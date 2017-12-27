bb00d3f6089881a18fcc282a2bb68a8a1c01f852
zhu <zhu@gmail.com>
2017-12-27 16:44:40 +0800

    编写启动第二页

---
+++ app/src/main/java/com/massky/sraumsmarthome/activity/LaunghSecondActivity.java
@@ -0,0 +1,36 @@
+package com.massky.sraumsmarthome.activity;
+
+import android.view.View;
+
+import com.massky.sraumsmarthome.base.BaseActivity;
+
+/**
+ * Created by zhu on 2017/12/27.
+ */
+
+ public class LaunghSecondActivity extends BaseActivity{
+    @Override
+    protected int viewId() {
+        return 0;
+    }
+
+    @Override
+    protected void onView() {
+
+    }
+
+    @Override
+    protected void onEvent() {
+
+    }
+
+    @Override
+    protected void onData() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}

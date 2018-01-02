b57dadb0cd7a29505c4148c64f5762043d9957aa
zhu <zhu@gmail.com>
2018-01-02 14:12:00 +0800

    添加搜索网关放大镜效果

---
+++ app/src/main/java/com/massky/sraumsmarthome/view/SycleSearchView.java
@@ -0,0 +1,24 @@
+package com.massky.sraumsmarthome.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by zhu on 2018/1/2.
+ */
+
+public class SycleSearchView extends View {
+    public SycleSearchView(Context context) {
+        super(context);
+    }
+
+    public SycleSearchView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SycleSearchView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
